56b83bcbfe7e247af3922e50213b6f1813c57b94
Dorothy Kessler <nj3dano@comcast.net>
2020-08-29 13:16:59 -0400

    test dak 8/29/20

--- mp0.shrubbery.py
+++ mp0.shrubbery.py
@@ -1,6 +1,6 @@
 # http://www.codeskulptor.org/#user29_UxVJ4lof6Z_0.py
 # program template for mini-project 0
-# changed extension to .py 8/29/2020
+# changed extension to .py 8/29/20
 
 # Modify the print statement according to 
 # the mini-project instructions
